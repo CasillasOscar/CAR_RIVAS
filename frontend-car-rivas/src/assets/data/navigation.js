fe958e92468e1d5011c7670172a3f8312ceaa573
CasillasOscar <oscarcasillasrodriguez@gmail.com>
2026-02-01 20:28:32 +0100

    feat: Create footer

---
+++ frontend-car-rivas/src/assets/data/navigation.js
@@ -0,0 +1,7 @@
+export const navLinks = [
+  { name: "Servicios", href: "#servicios" },
+  { name: "Tarifas", href: "#tarifas" },
+  { name: "Nosotros", href: "#nosotros" },
+  { name: "Instalaciones", href: "#instalaciones" },
+  { name: "Ubicación", href: "#ubicacion" },
+];
---
+++ frontend-car-rivas/src/assets/data/services.js
@@ -0,0 +1,5 @@
+export const services = [
+  { href: "#", label: "Entrenamiento Futbolistas" },
+  { href: "#", label: "Calistenia" },
+  { href: "#", label: "Entrenamiento Funcional" },
+];
