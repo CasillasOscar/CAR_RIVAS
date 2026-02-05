c871496a19ba85faec78733caa347b6dd16d3bdb
CasillasOscar <oscarcasillasrodriguez@gmail.com>
2026-02-05 19:25:23 +0100

    feat: Create section contact

---
+++ frontend-car-rivas/src/assets/data/schedules.js
@@ -0,0 +1,5 @@
+export const schedules = [
+  { day: "Lunes - Viernes", hours: "06:00 - 22:00" },
+  { day: "Sábados", hours: "08:00 - 20:00" },
+  { day: "Domingos", hours: "09:00 - 14:00" },
+];
---
+++ frontend-car-rivas/src/assets/data/schema_ld.js
@@ -0,0 +1,26 @@
+export const schema = {
+  "@context": "https://schema.org",
+  "@type": "HealthClub",
+  "name": "CAR RIVAS",
+  "image": "/images/about-coach.jpg",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "Calle Ejemplo 123",
+    "addressLocality": "Rivas-Vaciamadrid",
+    "addressRegion": "Madrid",
+    "postalCode": "28521",
+    "addressCountry": "ES"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 40.347, 
+    "longitude": -3.535
+  },
+  "url": "https://carrivas.es",
+  "telephone": "+34600000000",
+  "openingHoursSpecification": [
+    { "@type": "OpeningHoursSpecification", "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"], "opens": "06:00", "closes": "22:00" },
+    { "@type": "OpeningHoursSpecification", "dayOfWeek": "Saturday", "opens": "08:00", "closes": "20:00" },
+    { "@type": "OpeningHoursSpecification", "dayOfWeek": "Sunday", "opens": "09:00", "closes": "14:00" }
+  ]
+};
